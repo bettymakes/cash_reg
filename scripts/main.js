08704bcb3b91973e804172044d08f4ed4be48410
blibotics <blibotics@gmail.com>
2014-01-19 23:52:13 -0500

    Adding more functions and some simple bootstrap styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,34 @@
 var app = angular.module('cashRegApp', []);
 
 app.controller('MainCtrl', function($scope) {
-  
-  
-  $scope.addTax = function(amt){
-    var tax = 0.13;
-    var total = 0;
-    $scope.taxAmt = amt * tax;
-    $scope.totalAmt = amt * (1 + tax) ;
+  var tax = 0.13;
+  var total = 0;
+  var runningTax = 0;
+  var runningTotal = 0;  
+
+  $scope.totalAmt = 0;
+  $scope.itemArray = [];
+  $scope.itemArraywithTax = [];
+
+  $scope.calcTaxOnly = function(amt){
+    $scope.taxOnly = amt * tax;
+    return $scope.taxOnly;
+  };
+
+  $scope.calcTaxIncl = function(amt){
+    $scope.taxIncl = amt * (1 + tax);
+    $scope.itemArraywithTax.push($scope.taxIncl);
+  };
+
+  $scope.calcTotal = function(amt){
+    $scope.totalAmt += $scope.taxIncl;
+  }
+
+  $scope.addItem = function(amt){
+    $scope.itemArray.push(amt);
+    $scope.calcTaxOnly(amt);
+    $scope.calcTaxIncl(amt);
+    $scope.calcTotal(amt);
   };
 
   console.log("herro");
